99c0e84a71875c1fe42225e85d471d778e939c28
Giovana Napoli <giovana.ap.napoli@outlook.com>
2024-03-12 14:14:46 -0300

    feat: add express

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const routes = {
-  "/": "Curso de Node.js",
-  "/livros": "Rota de livros",
-  "/autores": "Rota de autores",
-};
-
-const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-type": "text/plain" });
-  response.end(routes[request.url]);
-});
-
-server.listen(PORT, () => {
-  console.log("Servidor escutando");
+app.listen(PORT, () => {
+  console.log("servidor escutando");
 });
