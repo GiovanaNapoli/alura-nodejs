a205db576b5a0fdf3bf2bbd3b30072e9dc9ab7d3
Giovana Napoli <giovana.ap.napoli@outlook.com>
2024-03-12 14:46:52 -0300

    feat: create get and post routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const livros = [
+  { id: 1, titulo: "Os sete maridos de Evelyn Hugo" },
+  { id: 2, titulo: "Rapido e devagar" },
+];
 
 app.get("/", (request, response) => {
   response.status(200).send("Curso de node.js");
+});
+
+app.get("/livros", (request, response) => {
+  response.status(200).json(livros);
+});
+
+app.post("/livros", (request, response) => {
+  livros.push(request.body);
+  response.status(200).send("Livro cadastrado com sucesso!");
 });
 
 export default app;
