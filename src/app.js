3f83065c29bc2a94990d900ebccc37fd972101c1
Giovana Napoli <giovana.ap.napoli@outlook.com>
2024-03-12 15:11:15 -0300

    feat: finish CRUD

--- src/app.js
+++ src/app.js
@@ -18,7 +18,31 @@ app.get("/livros", (request, response) => {
 
 app.post("/livros", (request, response) => {
   livros.push(request.body);
-  response.status(200).send("Livro cadastrado com sucesso!");
+  response.status(201).send("Livro cadastrado com sucesso!");
 });
+
+app.get("/livros/:id", (request, response) => {
+  const livro = getById(Number(request.params.id));
+  response.status(200).json(livro);
+});
+
+app.put("/livros/:id", (request, response) => {
+  const livro = getById(Number(request.params.id));
+  livro.titulo = request.body.titulo;
+  response.status(200).send(livros);
+});
+
+app.delete("/livros/:id", (request, response) => {
+  livros.splice(
+    livros.findIndex((l) => l.id === Number(request.params.id)),
+    1
+  );
+
+  response.status(204).send("Livro removido com sucesso");
+});
+
+function getById(id) {
+  return livros.find((l) => l.id === id);
+}
 
 export default app;
